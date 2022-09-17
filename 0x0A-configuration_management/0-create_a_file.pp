472a16c9ac6567db5b8757b1e022aed002296235
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-09-16 19:34:01 -0700

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # This manifest creates a file at /tmp
-file { 'Create_file':
+file { '/tmp/school':
     ensure  => 'file',
-    path    => '/tmp/school'
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
